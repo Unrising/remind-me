0fff92461fa9b850488a89386c177f1e97e2e0cf
Michael Picard <michael.picard14@gmail.com>
2025-07-14 00:23:17 +0200

    added multiple components + basic header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
